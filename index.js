159e28c8590d4ebc53be661d1d24347ce14c05cd
Sean-Jamieson <71718653+Sean-Jamieson@users.noreply.github.com>
2023-01-07 00:32:55 -0700

    migrating to glitch

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import itemRoutes from './routes/items.js';
-
+import dotenv from 'dotenv';
 const app = express();
 
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -14,13 +15,11 @@ app.use(cors());
 app.use('/items', itemRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello to shopify API');
+    res.send('Hello to inventory API');
 });
 
-const CONNECTION_URL = 'mongodb+srv://seanjamieson:jamieson105@cluster0.dtawl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT ;
 
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () =>console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(error.message));
